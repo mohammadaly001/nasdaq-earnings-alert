18c1c8ee876e073eaaaff0e44e8134718afdc86e
mohammadaly001 <aly957337@gmail.com>
2026-07-10 22:55:16 -0400

    Create nasdaq100.py

---
+++ nasdaq100.py
@@ -0,0 +1,51 @@
+NASDAQ100 = [
+    # Mega Cap Tech
+    "NVDA", "AAPL", "MSFT", "AMZN", "META",
+    "GOOGL", "GOOG", "TSLA", "AVGO", "NFLX",
+
+    # Semiconductors & AI Hardware
+    "AMD", "INTC", "MU", "ASML", "ARM",
+    "QCOM", "TXN", "AMAT", "LRCX", "KLAC",
+    "ADI", "MRVL", "NXPI", "MCHP",
+    "MPWR", "TER", "ALAB", "LITE",
+
+    # AI / Software / Cloud
+    "PLTR", "ADBE", "INTU",
+    "PANW", "CRWD", "FTNT", "SNPS", "CDNS",
+    "TEAM", "DDOG", "WDAY", "ADSK",
+    "SHOP", "APP",
+
+    # Internet / Consumer Tech
+    "COST", "PEP", "SBUX", "CMCSA",
+    "BKNG", "ABNB", "DASH", "MELI",
+    "PDD", "PYPL", "WMT",
+
+    # Healthcare / Biotech
+    "AMGN", "GILD", "VRTX", "REGN",
+    "ISRG", "IDXX", "ALNY", "DXCM",
+
+    # Energy / Infrastructure / AI Power
+    "CEG", "EXC", "AEP", "XEL",
+
+    # Space / New Additions
+    "SPCX", "RKLB", "NBIS", "CRWV",
+
+    # Memory / Storage
+    "SNDK", "WDC", "STX",
+
+    # Industrial / Other Nasdaq Names
+    "HON", "HONA", "ORLY", "CTAS", "ROST",
+    "FAST", "ODFL", "CSX", "AXON",
+    "PAYX", "ADP", "ROP", "PCAR", "TMUS", "MAR", "CSCO",
+
+    # Finance
+    "MSTR",
+
+    # Media / Gaming
+    "WBD", "EA", "TTWO",
+
+    # Other High Impact Names
+    "LIN", "MDLZ", "MNST", "KDP", "KHC",
+    "GEHC", "BKR", "FANG", "CCEP", "FER",
+    "TRI", "CPRT"
+]
